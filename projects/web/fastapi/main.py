d0e84038d696e1cad39d24c8b47e9ab588976b48
Amandeep Singh <adeepsingh2053@gmail.com>
2026-07-28 11:36:22 +0530

    post request

--- projects/web/fastapi/main.py
+++ projects/web/fastapi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-# Creating your fastapi application
+# Creating your fastapi application instance 
 app = FastAPI()
 
 #Simple fastapi code
@@ -9,7 +9,7 @@ app = FastAPI()
 def home():
     return{"Message":"Hi, Aman"}
 
-@app.get("/me")
+@app.get("/me") 
 def aboutme():
     return{
         "Name":"Amandeep",
@@ -19,10 +19,36 @@ def aboutme():
 
 
 @app.get("/users/{user_id}")
-def users(user_id: int):
+def users(user_id: str):
     return {
         "Hi":user_id,
     
     }
 
 
+
+@app.get("/userss/{user_id}")
+def get_user(user_id: int, active: bool):
+    return {
+        "user_id": user_id,
+        "active": active
+    }
+
+
+@app.get("/product/{product_id}")
+def product(product_id: int, price: int):
+    return {
+        "product_id": product_id,
+        "price": price
+    } 
+
+
+
+
+#  post request
+
+@app.post("/createuser")
+def create_user():
+    return{
+        "message":"User Created"
+    }
