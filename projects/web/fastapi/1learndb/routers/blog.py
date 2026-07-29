d630a5d8d48426d1485a036ca22710afa6c99587
Amandeep Singh <adeepsingh2053@gmail.com>
2026-07-29 11:54:18 +0530

    routers overview only

---
+++ projects/web/fastapi/1learndb/routers/blog.py
@@ -0,0 +1,27 @@
+# import  all the things 
+## use .. fror parent directory 
+
+#from fast api import APIRouter
+
+
+# router = APIRouter()
+
+
+#.... all the funcitonsa
+
+# @router.get(.....)
+
+
+
+
+
+# you can also do 
+
+
+
+
+''' router = APIRouter(
+tags = ['blogs']
+prefix "/blogs"
+
+)'''
---
+++ projects/web/fastapi/1learndb/routers/main.py
@@ -0,0 +1,11 @@
+# from . router import blog
+
+
+
+# models.Base.metadata.create_all(engine) ## to create table automatically
+
+
+
+#3 baki blog same as  before
+
+# app.include_router(blog.router)
